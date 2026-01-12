d9735b4f33a504bda0882a8b501dc74d78fd90a8
Christoph Hermann <christophhermann@Christophs-MacBook-Air.local>
2026-01-12 15:04:31 +0100

    Navbar: Option 1-3 entfernt, Option 4 zu 'Entwurf' umbenannt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,28 +12,10 @@ const Navbar = () => {
         </Link>
         <div className="navbar-links">
           <Link 
-            to="/" 
-            className={`navbar-link ${location.pathname === '/' ? 'active' : ''}`}
-          >
-            Option 1
-          </Link>
-          <Link 
-            to="/option2" 
-            className={`navbar-link ${location.pathname === '/option2' ? 'active' : ''}`}
-          >
-            Option 2
-          </Link>
-          <Link 
-            to="/option3" 
-            className={`navbar-link ${location.pathname === '/option3' ? 'active' : ''}`}
-          >
-            Option 3
-          </Link>
-          <Link 
             to="/option4" 
             className={`navbar-link ${location.pathname === '/option4' ? 'active' : ''}`}
           >
-            Option 4
+            Entwurf
           </Link>
           <Link 
             to="/galerie" 
